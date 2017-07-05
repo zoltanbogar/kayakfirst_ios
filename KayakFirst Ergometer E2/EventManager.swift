9fef823e1381f6b626010737a24fc38f59f21dac
Balazs Vidumanszki <balazs.vidumanszki@gmail.com>
2017-07-06 00:00:18 +0200

    Add - EventManager

--- KayakFirst Ergometer E2/EventManager.swift
+++ KayakFirst Ergometer E2/EventManager.swift
@@ -16,10 +16,32 @@ class EventManager: BaseManager {
         //private constructor
     }
     
+    //MARK: callback
+    var planEventCallback: ((_ data: [PlanEvent]?, _ error: Responses?) -> ())?
+    var eventDaysCallback: ((_ data: [Double]?, _ error: Responses?) -> ())?
+    
     //MARK: functions
-    func deleteEvent(event: Event, managerCallback: ((_ data: [Bool]?, _ error: Responses?) -> ())?) -> BaseManagerType {
-        //TODO
-        return EventManagerType.delete
+    func getEventDays() -> BaseManagerType {
+        let managerDownloadEventDays = ManagerDownloadEventDays()
+        runDownload(managerDownload: managerDownloadEventDays, managerCallBack: eventDaysCallback)
+        return EventManagerType.download_event_days
     }
     
+    func getEventByTimestamp(timestampFrom: Double, timestampTo: Double) -> BaseManagerType {
+        let managerDownloadEventByTimestamp = ManagerDownloadEventByTimestamp(timestampFrom: timestampFrom, timestampTo: timestampTo)
+        runDownload(managerDownload: managerDownloadEventByTimestamp, managerCallBack: planEventCallback)
+        return EventManagerType.download_event
+    }
+    
+    func saveEvent(event: Event, managerCallBack: ((_ data: Bool?, _ error: Responses?) -> ())?) -> BaseManagerType {
+        let managerModifyEventSave = ManagerModifyEventSave(data: event)
+        runModify(managerModify: managerModifyEventSave, managerCallBack: managerCallBack)
+        return EventManagerType.edit
+    }
+    
+    func deleteEvent(event: Event, managerCallback: ((_ data: Bool?, _ error: Responses?) -> ())?) -> BaseManagerType {
+        let managerModify = ManagerModifyEventDelete(data: event)
+        runModify(managerModify: managerModify, managerCallBack: managerCallback)
+        return EventManagerType.delete
+    }
 }
